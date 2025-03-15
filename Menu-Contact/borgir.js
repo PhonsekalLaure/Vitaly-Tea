d064bb11013161143301070e277e338a05160bc7
korkydoobie <202310065@fit.edu.ph>
2025-03-15 13:10:01 +0800

    Add files via upload

---
+++ Menu-Contact/borgir.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    hamburger.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
+$(document).ready(function() {
+    if (!sessionStorage.getItem('loadingShown')) {
+        $('body').load('load.html', function() {
+            setTimeout(function() {
+                sessionStorage.setItem('loadingShown', 'true');
+                location.reload();
+            }, 1000);
+        });
+    } else {
+        sessionStorage.removeItem('loadingShown');
+    }
+});
+
